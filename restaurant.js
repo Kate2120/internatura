98c7dcc6f2745d6babd660ea9ec8f6a88bd7fa99
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-11-12 06:16:48 +0200

    Create restaurant.js

---
+++ restaurant.js
@@ -0,0 +1,47 @@
+class Department {
+    constructor(name, id, amount) {
+        this.name = name;
+        this.id = id;
+        this.amount = amount;
+    }
+
+}
+
+class Positions {
+    constructor(id, name, salary, department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+
+    }
+
+}
+
+let departmentsID = [
+    {id: 1,
+     link: administration,
+    },
+    {id: 2,
+     link: administration,
+    },
+    {id: 3,
+     link: administration,
+    },
+    {id: 4,
+    link: administration,
+    },
+    {id: 5,
+    link: administration,
+    },
+    {id: 6,
+    link: administration,
+    },
+    {id: 7,
+    link: administration,
+    },
+    ]
+
+function getDepartmentById (argument, ){
+    for(let )
+}

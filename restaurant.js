9ac4e3031bc5395b4ac5b0e37dba1da5d1b8084e
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-11-12 10:09:07 +0200

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,11 +1,4 @@
-class Department {
-    constructor(name, id, amount) {
-        this.name = name;
-        this.id = id;
-        this.amount = amount;
-    }
 
-}
 
 class Positions {
     constructor(id, name, salary, department) {
@@ -13,35 +6,43 @@ class Positions {
         this.name = name;
         this.salary = salary;
         this.department = department;
+        this.
 
     }
 
 }
 
+let admin = new Positions(7, "Администратор", 30000, 8);
+let waiter = new Positions(8, "Официант", 20000, 8);
+let security = new Positions(7, "Охранник", 25000, 8);
+let cook = new Positions(7, "Повар", 30000, 8);
+let cleaner = new Positions(7, "Уборщик", 12000, 8);
+let accountant = new Positions(7, "Бухгалтер", 12000, 8);
+console.log(admin);
+console.log(waiter);
+console.log(security);
+console.log(cook);
+console.log(cleaner);
 let departmentsID = [
     {id: 1,
-     link: administration,
+        link: administration,
     },
     {id: 2,
-     link: administration,
+        link: administration,
     },
     {id: 3,
-     link: administration,
+        link: administration,
     },
     {id: 4,
-    link: administration,
+        link: administration,
     },
     {id: 5,
-    link: administration,
+        link: administration,
     },
     {id: 6,
-    link: administration,
+        link: administration,
     },
     {id: 7,
-    link: administration,
+        link: administration,
     },
-    ]
-
-function getDepartmentById (argument, ){
-    for(let )
-}
+]

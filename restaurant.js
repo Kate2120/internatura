aae9d415645b063152eec263408166e048ffc2cb
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-11-15 21:02:25 +0200

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,20 +1,37 @@
 class Department {
-    constructor(departmentId, title, numberEmployees){
+    constructor(departmentId, title, arrayEmployees){
         this.departmentId = departmentId;
         this.title = title;
-        this.numberEmployees = numberEmployees;
+        this.arrayEmployees = arrayEmployees;
+    }
+    [Symbol.iterator] () {
+        let objKeys = Object.keys(this);
+        let limit = objKeys.length;
+        let counter = 0;
+        let thisDepartment = this;
+        return {
+            next() {
+                if(counter < limit){
+               return { done: false,
+                    value: thisDepartment[objKeys[counter++]]
+               }
+                }
+                return {
+                done: true,
+                    value: undefined
+                }
+            }
+        }
     }
 
 
 }
 
-let kitchen = new Department(1, "Кухня", 5);
-/*console.log(kitchen);*/
-let hall = new Department(2, "Зал", 7);
-let financial = new Department(3, "Финансовый отдел", 2);
-/*console.log(kitchen);
-console.log(hall);
-console.log(financial);*/
+let kitchen = new Department(1, "Кухня", arrayKitchen = []);
+
+let hall = new Department(2, "Зал", arrayHall = []);
+let financial = new Department(3, "Финансовый отдел", arrayFinancial = []);
+
 
 class Positions {
     constructor(id, title, salary, head) {
@@ -23,6 +40,25 @@ class Positions {
         this.salary = salary;
         this.head = head;
     }
+    [Symbol.iterator] () {
+        let objKeys = Object.keys(this);
+        let limit = objKeys.length;
+        let counter = 0;
+        let thisPositions = this;
+        return {
+            next() {
+                if(counter < limit){
+                    return { done: false,
+                        value: thisPositions[objKeys[counter++]]
+                    }
+                }
+                return {
+                    done: true,
+                    value: undefined
+                }
+            }
+        }
+    }
 
 }
 
@@ -30,75 +66,12 @@ let adminHall = new Positions(21, "Администратор", 30000, true);
 let waiter = new Positions(22, "Официант", 20000, false);
 let security = new Positions(23, "Охранник", 25000, false);
 let cook = new Positions(11, "Повар", 30000, false);
-let cookHelper = new Positions(12, "Повар", 15000,  false);
+let cookHelper = new Positions(12, "Помощник повара", 15000,  false);
 let cookHead = new Positions(13, "Шеф-повар", 30000,  true);
 let cleaner = new Positions(24, "Уборщик", 12000, false);
 let accountant = new Positions(32, "Бухгалтер", 12000,  false);
 let accountantHead = new Positions(31, "Главный бухгалтер", 12000, true);
-let accountantHelper = new Positions(33, "Помошник бухгалтера", 10000, false);
-
-let departments = [
-    {kitchen: {departmentId: 1, title: 'Кухня',  employees: [
-                {employeeId: 111, name: 'Нина', surname: 'Малыгина', age: 43, employmentDate: '20.07.2021', position: cookHead, dismissalDate: null},
-                {employeeId: 112, name: 'Ольга', surname: 'Савенкова', age: 35, employmentDate: '03.06.2021', position: cook, dismissalDate: null},
-                {employeeId: 113, name: 'Марина', surname: 'Митина', age: 25, employmentDate: '14.06.2021', position: cookHelper, dismissalDate: null}]}},
-    {hall:{departmentId: 2, title: 'Зал', employees: [
-                {employeeId: 211, name: 'Александра', surname: 'Синицина', age: 21, employmentDate: '20.06.2021', position: waiter, dismissalDate: null},
-                {employeeId: 212, name: 'Ольга', surname: 'Валова', age: 35, employmentDate: '20.06.2021', position: waiter, dismissalDate: null},
-                {employeeId: 213, name: 'Антонина', surname: 'Сидорова', age: 25, employmentDate: '21.06.2021', position: adminHall, dismissalDate: null},
-                {employeeId: 214, name: 'Елизавета', surname: 'Кукушкина', age: 31, employmentDate: '18.06.2021', position: cleaner, dismissalDate: null},
-                {employeeId: 215, name: 'Максим', surname: 'Ветров', age: 28, employmentDate: '16.06.2021', position: security, dismissalDate: null},
-                {employeeId: 215, name: 'Петр', surname: 'Соломатин', age: 24, employmentDate: '14.06.2021', position: security, dismissalDate: '20.06.2021'}]}},
-    {financial: {departmentId: 3, title: 'Финансовый отдел', employees: [
-                {employeeId: 311, name: 'Елена', surname: 'Попова', age: 28, employmentDate: '20.06.2021', position: accountantHead, dismissalDate: null},
-                {employeeId: 312, name: 'Ольга', surname: 'Иванова', age: 28, employmentDate: '03.06.2021', position: accountant, dismissalDate: null},
-                {employeeId: 313, name: 'Марина', surname: 'Петрова', age: 28, employmentDate: '11.06.2021', position: accountantHelper, dismissalDate: '14.06.2021'},
-                {employeeId: 314, name: 'Наталия', surname: 'Сачкова', age: 28, employmentDate: '15.06.2021', position: accountantHelper, dismissalDate: null}]}}
-];
-
-console.log(departments);
-function allSalaries(argument){
-    for (let item of argument){
-
-        for(let key in item){
-
-            if(typeof item[key] === 'object'){
-            for(let element in item[key]){
-
-                if(typeof item[key][element] === 'object'){
-                    for(let value in item[key][element]){
-                        
-                        if(typeof item[key][element][value] === 'object'){
-                            for(let el in item[key][element][value]){
-
-                                if(typeof item[key][element][value] === 'object'){
-                                    if(typeof item[key][element][value][el] === 'object'){
-                                        for(let data in item[key][element][value][el]){
-                                            console.log(item[key][element][value][el][data]);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        }
-    }
-}
-console.log(allSalaries(departments));
-
-let positionsAll = [
-    adminHall,
-    waiter,
-    security,
-    cook,
-    cookHead,
-    cleaner,
-    accountant,
-    accountantHead,
-]
+let accountantHelper = new Positions(33, "Помощник бухгалтера", 10000, false);
 
 class Employee {
     constructor(employeeId, name, surname, age, employmentDate, position, dismissalDate) {
@@ -111,26 +84,130 @@ class Employee {
         this.dismissalDate = dismissalDate;
 
     }
-    getInfoPosition(position){
-        for(let item of positionsAll) {
-            if(this.position === item.title){
-                return item;
+    [Symbol.iterator] () {
+        let objKeys = Object.keys(this);
+        let limit = objKeys.length;
+        let counter = 0;
+        let thisEmployee = this;
+        return {
+            next() {
+                if(counter < limit){
+                    return { done: false,
+                        value: thisEmployee[objKeys[counter++]]
+                    }
+                }
+                return {
+                    done: true,
+                    value: undefined
+                }
             }
         }
     }
 
 }
 
-let employer = new Employee(30,"Елена", "Попова", 28, "20.06.2021", "Официант", null);
+function makeUsersArray(employeeId, name, surname, age, employmentDate, position, dismissalDate){
 
-/*
-console.log(employer);
-console.log(employer.getInfoPosition("Официант"));
-*/
+    if(position === "Шеф-повар" || position === "Повар" || position === "Помощник повара"){
+switch(position){
+    case "Шеф-повар": [position] = cookHead;
+        break;
+    case "Повар": [position] = cook;
+        break;
+    case "Помощник повара": [position] = cookHelper;
+        break;
+}
+        let employer = new Employee(employeeId, name, surname, age, employmentDate, position, dismissalDate);
+        arrayKitchen.push(employer);
 
-let employees = [
-    {employeeId: 30, name: 'Елена', surname: 'Попова', age: 28, employmentDate: '20.06.2021', position: waiter, dismissalDate: null},
-    {employeeId: 31, name: 'Ольга', surname: 'Иванова', age: 28, employmentDate: '03.06.2021', position: waiter, dismissalDate: null},
-    {employeeId: 32, name: 'Марина', surname: 'Петрова', age: 28, employmentDate: '14.06.2021', position: waiter, dismissalDate: null}
-];
-/*console.log(employees);*/
+        return arrayKitchen;
+    } else if(position === "Официант" || position === "Администратор" || position === "Уборщик" || position === "Охранник"){
+        switch(position){
+            case "Официант": [position] = waiter;
+                break;
+            case "Администратор": [position] = adminHall;
+                break;
+            case "Уборщик": [position] = cleaner;
+                break;
+            case "Охранник": [position] = security;
+                break;
+        }
+        let employer = new Employee(employeeId, name, surname, age, employmentDate, position, dismissalDate);
+        arrayHall.push(employer);
+
+        return arrayHall;
+    } else if(position === "Главный бухгалтер" || position === "Бухгалтер" || position === "Помощник бухгалтера"){
+        switch(position){
+            case "Главный бухгалтер": [position] = accountantHead;
+                break;
+            case "Бухгалтер": [position] = accountant;
+                break;
+            case "Помощник бухгалтера": [position] = accountantHelper;
+                break;
+
+        }
+
+        let employer = new Employee(employeeId, name, surname, age, employmentDate, position, dismissalDate);
+        arrayFinancial.push(employer);
+
+        return arrayFinancial;
+    }
+
+
+}
+makeUsersArray(111, 'Нина', 'Малыгина', 43, '20.07.2021', "Шеф-повар", "");
+makeUsersArray(112, 'Ольга', 'Савенкова', 35, '03.06.2021', "Повар", "");
+makeUsersArray(113, 'Марина', 'Митина', 25, '14.06.2021', "Помощник повара", "");
+makeUsersArray(211, 'Александра', 'Синицина', 35, '20.06.2021', "Официант", "");
+makeUsersArray(212, 'Ольга', 'Валова', 25, '14.06.2021', "Официант", "");
+makeUsersArray(213, 'Антонина', 'Сидорова', 25, '21.06.2021', "Администратор", "");
+makeUsersArray(214, 'Елизавета', 'Кукушкина', 31, '18.06.2021', "Уборщик", "");
+makeUsersArray(215, 'Максим', 'Ветров', 28, '16.06.2021', "Охранник", "");
+makeUsersArray(215, 'Петр', 'Соломатин', 24, '14.06.2021', "Охранник", '20.06.2021');
+makeUsersArray(311, 'Елена', 'Попова', 28, '20.06.2021', "Главный бухгалтер", "" );
+makeUsersArray(312, 'Ольга', 'Иванова', 29, '03.06.2021', "Бухгалтер", "" );
+makeUsersArray(313, 'Марина', 'Петрова', 37, '11.06.2021', "Помощник бухгалтера", '14.06.2021' );
+makeUsersArray(314, 'Наталия', 'Сачкова', 32, '15.06.2021', "Помощник бухгалтера", "" );
+
+let departments = [kitchen,hall,financial];
+console.log(departments);
+
+function allSalaries(argument, sum) {
+    console.log(argument);
+    sum = sum || 0;
+     if(typeof argument === 'object') {
+        for(let element of argument){
+
+            if(element.hasOwnProperty("salary")){
+                sum += element.salary;
+
+            }
+            argument = element;
+        }
+         return allSalaries(argument, sum);
+    }
+    if(Array.isArray(argument)) {
+      argument.forEach(function (item){
+          argument = item;
+          return allSalaries(argument, sum);
+      })
+    }
+    return sum;
+}
+console.log(allSalaries(departments));
+
+class A{
+    data;
+    constructor() {
+        this,data = [];
+    }
+
+    add(item){
+        if(!validItem(item)){
+            throw new Error("item not valid");
+        }
+
+        this.push(item);
+    }
+
+}

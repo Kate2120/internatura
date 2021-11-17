1efdfa0ecb1e077240a162ca436b13257339e4bd
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-11-17 09:45:46 +0200

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,123 +1,60 @@
-class Restaurant {
-    constructor(){
+let restaurant = [];
+class DepartmentRestaurant {
+    arrayPositions = [];
+    constructor(departmentId, title){
+        this.departmentId = departmentId;
+        this.title = title;
 
-    }
-    [Symbol.iterator] () {
-        let objKeys = Object.keys(this);
-        let limit = objKeys.length;
-        let counter = 0;
-        let thisPositions = this;
-        return {
-            next() {
-                if(counter < limit){
-                    return { done: false,
-                        value: thisPositions[objKeys[counter++]]
-                    }
-                }
-                return {
-                    done: true,
-                    value: undefined
-                }
-            }
-        }
-    }
-}
-let departmentsRestaurant = new Restaurant();
-let currentId;
-function takeId(id){
-    let currentId = id[0];
-    id = id.filter((function(item) {
-        return item !== id[0];
-    }))
-    console.log(id);
-    return currentId;
-}
-
-
-class RestaurantDepartments {
-positions = [];
-    constructor(title) {
-this.title = title;
-    }
-    [Symbol.iterator] () {
-        let objKeys = Object.keys(this);
-        let limit = objKeys.length;
-        let counter = 0;
-        let thisPositions = this;
-        return {
-            next() {
-                if(counter < limit){
-                    return { done: false,
-                        value: thisPositions[objKeys[counter++]]
-                    }
-                }
-                return {
-                    done: true,
-                    value: undefined
-                }
-            }
-        }
     }
 
 }
 
-
-function addDepartment(title){
-    let department = new RestaurantDepartments(title);
-    let key = title;
-    departmentsRestaurant[key] = department;
-    return departmentsRestaurant;
+function addDepartment(departmentId, title){
+    let department = new DepartmentRestaurant(departmentId, title);
+    restaurant.push(department);
+    return department;
 }
-
-addDepartment("Кухня");
-addDepartment("Зал");
-addDepartment("Финансовый");
+addDepartment(1, "Кухня");
+addDepartment(2, "Зал");
+addDepartment(3, "Финансовый");
+console.log(restaurant);
 
 class Positions {
-    employee = [];
+    arrayEmployee = [];
     constructor(department, id, title, salary, head) {
         this.id = id;
         this.title = title;
         this.salary = salary;
         this.head = head;
     }
-    [Symbol.iterator] () {
-        let objKeys = Object.keys(this);
-        let limit = objKeys.length;
-        let counter = 0;
-        let thisPositions = this;
-        return {
-            next() {
-                if(counter < limit){
-                    return { done: false,
-                        value: thisPositions[objKeys[counter++]]
-                    }
-                }
-                return {
-                    done: true,
-                    value: undefined
-                }
-            }
-        }
-    }
-    amountEmployee(){
-        return employee.length;
-    }
-    positionSalary(){
+    salaryAmount(){
         return this.salary;
     }
-
+  /*  positionAmount() {
+        return this.arrayEmployee.length;
+    }*/
+    ooo(){
+        let counter = 0;
+        for(let i = 0; i < this.arrayEmployee.length; i++){
+            if(this.arrayEmployee[i].dismissalDate === ""){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
-
-
-
 
 function addPositions(department, id, title, salary, head) {
-   let position = new Positions(department, id, title, salary, head);
-    departmentsRestaurant[department].positions.push(position);
-return departmentsRestaurant;
+    let position = new Positions(department, id, title, salary, head);
+    for(let i = 0; i < restaurant.length; i++){
+        console.log(restaurant[i]);
+        if(restaurant[i].title === department){
+            restaurant[i].arrayPositions.push(position);
+        }
+    }
+    return position;
 }
-addPositions("Кухня", 19, "Повар", 30000, false );
+
 addPositions("Зал", 21, "Администратор", 30000, true);
 addPositions("Зал", 22, "Официант", 20000, false);
 addPositions("Зал", 23, "Охранник", 25000, false);
@@ -130,7 +67,7 @@ addPositions("Финансовый", 31, "Главный бухгалтер", 12
 addPositions("Финансовый", 33, "Помощник бухгалтера", 10000, false);
 
 class Employee {
-    constructor(position, employeeId, name, surname, age, employmentDate, dismissalDate) {
+    constructor(department, position, employeeId, name, surname, age, employmentDate, dismissalDate) {
         this.employeeId = employeeId;
         this.name = name;
         this.surname = surname;
@@ -138,69 +75,58 @@ class Employee {
         this.employmentDate = employmentDate;
         this.position = position;
         this.dismissalDate = dismissalDate;
+        this.department = department;
 
     }
-    [Symbol.iterator] () {
-        let objKeys = Object.keys(this);
-        let limit = objKeys.length;
-        let counter = 0;
-        let thisEmployee = this;
-        return {
-            next() {
-                if(counter < limit){
-                    return { done: false,
-                        value: thisEmployee[objKeys[counter++]]
-                    }
-                }
-                return {
-                    done: true,
-                    value: undefined
+    dismiss(){
+        return this.dismissalDate;
+    }
+
+}
+function addEmployee(department, position, employeeId, name, surname, age, employmentDate, dismissalDate) {
+    let employer = new Employee(department, position, employeeId, name, surname, age, employmentDate, dismissalDate);
+    for(let i = 0; i < restaurant.length; i++){
+
+        if(restaurant[i].title === department){
+            console.log(restaurant[i]);
+            for(let j = 0; j < restaurant[i].arrayPositions.length; j++){
+                if(restaurant[i].arrayPositions[j].title === position){
+                    restaurant[i].arrayPositions[j].arrayEmployee.push(employer);
                 }
             }
         }
     }
-
+    return employer;
 }
-function addEmployee(employeeId, name, surname, age, employmentDate, position, dismissalDate) {
-    let employer = new Employee(position, employeeId, name, surname, age, employmentDate, dismissalDate);
-    for(let item of departmentsRestaurant){
-        for(let element of item){
-            for(let value of element) {
-                if(value.title === position) {
-                    value.employee.push(employer);
-                }
-            }
-        }
-    }
-    return departmentsRestaurant;
-}
-addEmployee(111, 'Нина', 'Малыгина', 43, '20.07.2021', "Шеф-повар", "");
-addEmployee(112, 'Ольга', 'Савенкова', 35, '03.06.2021', "Повар", "");
-addEmployee(113, 'Марина', 'Митина', 25, '14.06.2021', "Помощник повара", "");
-addEmployee(211, 'Александра', 'Синицина', 35, '20.06.2021', "Официант", "");
-addEmployee(212, 'Ольга', 'Валова', 25, '14.06.2021', "Официант", "");
-addEmployee(213, 'Антонина', 'Сидорова', 25, '21.06.2021', "Администратор", "");
-addEmployee(214, 'Елизавета', 'Кукушкина', 31, '18.06.2021', "Уборщик", "");
-addEmployee(215, 'Максим', 'Ветров', 28, '16.06.2021', "Охранник", "");
-addEmployee(215, 'Петр', 'Соломатин', 24, '14.06.2021', "Охранник", '20.06.2021');
-addEmployee(311, 'Елена', 'Попова', 28, '20.06.2021', "Главный бухгалтер", "" );
-addEmployee(312, 'Ольга', 'Иванова', 29, '03.06.2021', "Бухгалтер", "" );
-addEmployee(313, 'Марина', 'Петрова', 37, '11.06.2021', "Помощник бухгалтера", '14.06.2021' );
-addEmployee(314, 'Наталия', 'Сачкова', 32, '15.06.2021', "Помощник бухгалтера", "" );
-console.log(departmentsRestaurant);
+addEmployee("Кухня", "Шеф-повар",111, 'Нина', 'Малыгина', 43, '20.07.2021', "");
+addEmployee("Кухня", "Повар", 112, 'Ольга', 'Савенкова', 35, '03.06.2021', "");
+addEmployee("Кухня", "Помощник повара", 113, 'Марина', 'Митина', 25, '14.06.2021', "");
+addEmployee("Зал", "Официант", 211, 'Александра', 'Синицина', 35, '20.06.2021', "");
+addEmployee("Зал", "Официант", 212, 'Ольга', 'Валова', 25, '14.06.2021', "");
+addEmployee("Зал", "Администратор", 213, 'Антонина', 'Сидорова', 25, '21.06.2021', "");
+addEmployee("Зал", "Уборщик", 214, 'Елизавета', 'Кукушкина', 31, '18.06.2021', "");
+addEmployee("Зал", "Охранник", 215, 'Максим', 'Ветров', 28, '16.06.2021', "");
+addEmployee("Зал", "Охранник", 215, 'Петр', 'Соломатин', 24, '14.06.2021', '20.06.2021');
+addEmployee("Финансовый", "Главный бухгалтер", 311, 'Елена', 'Попова', 28, '20.06.2021', "" );
+addEmployee("Финансовый", "Бухгалтер", 312, 'Ольга', 'Иванова', 29, '03.06.2021', "" );
+addEmployee("Финансовый", "Помощник бухгалтера", 313, 'Марина', 'Петрова', 37, '11.06.2021', '14.06.2021' );
+addEmployee("Финансовый", "Помощник бухгалтера", 314, 'Наталия', 'Сачкова', 32, '15.06.2021', "" );
+console.log(restaurant);
 
-function salary(argument) {
+function salaryDepartment(department){
     let sum = 0;
-    let allsalary = 0;
-    for(let item of departmentsRestaurant) {
-        console.log(item.positions);
-        for (let i = 0; i < item.positions.length; i++) {
-            console.log(item.positions[i].employee.length);
-            console.log(item.positions[i].positionSalary());
-            allsalary = item.positions[i].employee.length * item.positions[i].positionSalary()
-            sum += allsalary;
+    for(let i = 0; i < restaurant.length; i++){
+if(restaurant[i].title === department){
+
+        console.log(restaurant[i]);
+        for(let j = 0; j < restaurant[i].arrayPositions.length; j++){
+
+            sum += restaurant[i].arrayPositions[j].salaryAmount() * restaurant[i].arrayPositions[j].ooo();
+            console.log(sum);
         }
+}
+
     }
     return sum;
 }
-salary(departmentsRestaurant);
+console.log(salaryDepartment("Финансовый"));

e2e7b7b89c6673f7eec04e7cd50c7974fcda8b63
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-11-23 10:44:40 +0200

    Update bank.js

--- bank.js
+++ bank.js
@@ -1,12 +1,9 @@
-
-
 class Client{
     firstname;
     surname;
     constructor(){
 
     }
-
     set firstName(value) {
         this.firstname = value;
     }
@@ -59,35 +56,47 @@ class Bank {
     }
 
     addBills(clientId, bill){
-        for(this.clients of client){
+        for(let client of this.clients){
             if(client.id === clientId){
                 client.bills.push(bill);
             }
         }
     }
+    async getCallbackValue(currency, typeRate){
+
+        let response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+        let data = await response.json();
+        for(let i = 0; i < data.length; i++){
+            if(data[i].ccy === currency){
+               let rate = await data[i][typeRate];
+                console.log(rate);
+            }
+        }
+       /* callback(data);*/
+        console.log(data);
+        return rate;
+    }
 
     budget(isActive, currency){
         let sum = 0;
         for(let item of this.clients){
+            console.log(item);
             if(item.is_active === isActive) {
-                for (let element of item.bills) {
-                    for(let i = 0; i < data.length; i++){
-                        if(data[i].currency === currency){
-                            console.log(item);
-                            sum += element.amount * data[i].buy;
-                        }
-                    }
-
-
+                console.log(item.is_active);
+                console.log(isActive);
+                console.log(item.bills.length);
+                for (let i = 0; i < item.bills.length; i++) {
+                            console.log(item.bills[i]);
+                            console.log(item.bills[i].amount);
+                            console.log(data);
+                            sum += item.bills[i].amount * rate;
+                            console.log(sum);
                 }
             }
         }
         return sum;
     }
 
-    f(){
-
-    }
 }
 let myBank = new Bank();
 console.log(myBank);
@@ -95,18 +104,21 @@ myBank.addClients(client1);
 /*myBank.addBills(1,bill);*/
 
 
-myBank.budget(true, 'USD');
 
-async function getCallbackValue(callback){
+myBank.addBills(1, bill);
+myBank.budget(true, 'USD');
+console.log(myBank.getCallbackValue('USD', 'buy'));
+/*myBank.getCallbackValue('USD', 'buy');*/
+/*async function getCallbackValue(callback){
     let response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
     let data = await response.json();
     callback(data);
 
     console.log(data);
-}
+}*/
 
+/*
 function getRate(data){
     return data;
 }
-getCallbackValue(getRate);
-
+getCallbackValue(getRate);*/

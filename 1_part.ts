5b848cd1f3ffb875d4cfdfa39c8d42363e869875
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-12-02 18:20:44 +0200

    Update 1_part.ts

--- 1_part.ts
+++ 1_part.ts
@@ -51,3 +51,63 @@ function anagram(word1: string, word2: string, i: number, j: number, countWord1:
     }
     return true;
 }
+
+interface Memo {
+    words?: string[],
+    result?: boolean,
+}
+let memoisedAnangram = (function() {
+    let memo: Memo = {
+        words: [],
+    };
+    
+    let counter = 0;
+    return function anagram(word1: string, word2: string, i: number, j: number, countWord1: number, countWord2: number): boolean | undefined {
+        countWord1 = countWord1 || 0;
+        countWord2 = countWord2 || 0;
+        i = i || 0;
+        j = j || 0;
+        let arr = [word1, word2];
+        
+        for(let key in memo.words){
+            for(let i = 0; i < arr.length; i++){
+                if(key === arr[i]){
+                    counter++;
+                }
+            }
+            if(counter === key.length){
+                return memo.result;
+            }
+        }
+        
+        if(word1.length === word2.length){
+           
+        
+                if( i < word1.length) {
+                    if(j < word1.length) {
+                        if (word1[i] === word2[j]) {
+                            countWord1++;
+                            if (word1[i] === word1[j]) {
+                                countWord2++;
+                            }
+                        } else if (word1[i] === word1[j]) {
+                            countWord2++;
+                        }
+                        return anagram(word1, word2,i,j + 1, countWord1, countWord2);
+                    }
+                    return anagram(word1, word2,i + 1,0, countWord1, countWord2);
+                }
+                if (countWord1 !== countWord2) {
+                    memo.words = arr;
+                    memo.result = false;
+                    return false;
+                }
+                memo.words = arr;
+                memo.result = true;
+                return true;
+                }
+                 memo.words = arr;
+                 memo.result = false;
+                 return false;
+
+            }})();

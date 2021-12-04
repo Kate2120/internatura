e139c54aee1ef2f8f9f2f5ba0af0ab8289165a3c
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-12-04 13:37:00 +0200

    Update part_2.ts

--- part_2.ts
+++ part_2.ts
@@ -16,3 +16,20 @@ custom.prototype.customBind = function (context: Context){
 
 }
 
+interface Func {
+    [index : string]: any;
+}
+interface Context {
+    [index : symbol]: any;
+}
+let custom: Func = Function;
+custom.prototype.customCall = function (context: Context){
+    let key: symbol = Symbol();
+    context[key] = this;
+    return (function() {
+        let result = context[key]();
+        delete context[key];
+        return result;
+    })();
+
+}

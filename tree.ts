4c70b6b72a2dadcbd1f48effd3729899a64c387e
Kate2120 <82631629+Kate2120@users.noreply.github.com>
2021-12-07 11:49:27 +0200

    Update tree.ts

--- tree.ts
+++ tree.ts
@@ -1,5 +1,16 @@
+function dataHash(value){
+    let hash = 0;
+    if (value.length === 0) return hash;
+    for (let i = 0; i < value.length; i++) {
+        let char = value.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash;
+    }
+    return hash;
+}
+
 class Tree {
-    value: number | null;
+    value: any;
     left: null | Tree;
     right: null | Tree;
 
@@ -9,30 +20,30 @@ class Tree {
         this.right = null;
     }
 
-    add(value: number | null, node: Tree): Tree {
+    add(value: any, node: Tree): Tree {
+        value = dataHash(value);
         node = node || this;
 
         if(node.value === null){
             node.value = value;
             return;
         }else {
-            if(node.value !== null && value !== null){
                 if(node.value > value){
                     if(node.left === null){
                         node.left = new Tree();
                     }
                     return this.add(value, node.left);
                 }
-            }
+
         }
         if(node.right === null){
             node.right = new Tree();
         }
         return this.add(value, node.right);
     }
-    search(value: number | null, node: Tree): Tree {
+    search(value: number, node: Tree): Tree {
         node = node || this;
-        if(node.value !== null && value !== null){
+        if(node.value !== null){
             if(value < node.value) {
                 return this.search(value, node.left);
             } else{
@@ -58,30 +69,31 @@ class Tree {
         if(node.value === value){
             if(node.right === null && node.left === null){
                 node = null;
-                return node;
+                return;
             }
             if(node.right === null){
                 node = node.left;
-                return node;
+                return;
             } else if(node.left === null){
                 node = node.right;
-                return node;
+                return;
             }
             let newNode = this.minNode(node.right);
             node.value = newNode.value;
             node.right = this.delite(newNode.value, node.right,);
-            return node;
+            return;
         }
         if(value !== null){
             if(value < node.value) {
                 node.left = this.delite(value, node.left);
-                return node;
+                return;
             }
             node.right = this.delite(value, node.right);
-            return node;
+            return;
         }
     }
 }
+
 
 
 interface Array<T> {
